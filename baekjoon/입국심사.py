d791abda96cf6b366f451168633b3cbf53b4c303
soonsuyeon <soocy0718@gmail.com>
2023-04-20 23:35:40 +0900

    Update 23.04.20

---
+++ baekjoon/입국심사.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+data = []
+for _ in range(n):
+    num = int(input())
+    data.append(num)
+
+start = 0
+end = max(data) * m
+while start <= end:
+    mid = (start+end) // 2
+    total = 0
+    for i in range(n):
+        total += mid // data[i]
+    if total < m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(start)
+
+# 좀 더 최적화하기
+input = sys.stdin.readline
+n, m = map(int, input().split())
+data = []
+for _ in range(n):
+    num = int(input())
+    data.append(num)
+
+start = 1
+end = max(data) * m
+data.sort()
+while start <= end:
+    mid = (start+end) // 2
+    total = 0
+    for i in range(n):
+        total += mid // data[i]
+        if total > m:
+            break
+    if total < m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(start)

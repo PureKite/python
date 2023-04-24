35eb354dd390124567dbfab22cd24f20210793e4
Suyeon Soon <48740214+soocy0718@users.noreply.github.com>
2023-04-24 18:04:24 +0900

    [Silver III] Title: 두 스티커, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16937. 두 스티커/두 스티커.py
@@ -0,0 +1,24 @@
+import sys
+from itertools import permutations
+input = sys.stdin.readline
+
+h, w = map(int, input().split())
+n = int(input())
+data = []
+for _ in range(n):
+    r, c = map(int, input().split())
+    data.append((r, c))
+sticker = (list(permutations(data, 2)))
+result = 0
+for i, j in sticker:
+    r1, c1 = i[0], i[1]
+    r2, c2 = j[0], j[1]
+    if (r1 + r2 <= h and max(c1, c2) <= w) or (max(r1, r2) <= h and c1 + c2 <= w):
+        result = max(result, r1*c1 + r2*c2)
+    if (c1 + r2 <= h and max(r1, c2) <= w) or (max(c1, r2) <= h and r1 + c2 <= w):
+        result = max(result, r1*c1 + r2*c2)
+    if (c1 + c2 <= h and max(r1, r2) <= w) or (max(c1, c2) <= h and r1 + r2 <= w):
+        result = max(result, r1*c1 + r2*c2)
+    if (r1 + c2 <= h and max(c1, r2) <= w) or (max(r1, c2) <= h and c1 + r2 <= w):
+        result = max(result, r1*c1 + r2*c2)
+print(result)

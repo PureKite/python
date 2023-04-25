b2d942834fafd9a738acb6ce046d2f3525581798
Suyeon Soon <48740214+soocy0718@users.noreply.github.com>
2023-04-25 15:41:43 +0900

    [Silver I] Title: 포도주 시식, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+grape = list(int(input()) for _ in range(n))
+dp = [0]*n
+dp[0] = grape[0]
+
+if n > 1:
+    dp[1] = grape[0] + grape[1]
+if n > 2:
+    dp[2] = max(grape[2]+grape[1], grape[2]+grape[0], dp[1])
+
+for i in range(3, n):
+        dp[i] = max(dp[i-1], dp[i-3]+grape[i-1]+grape[i], dp[i-2]+grape[i])
+print(dp[n-1])
